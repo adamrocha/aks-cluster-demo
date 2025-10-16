780271515b8f15826a89c14be072b4c5c2298d28
Adam Rocha <adamrocha13@gmail.com>
2025-10-15 23:00:07 -0700

    Add Terraform configuration for AKS cluster infrastructure

---
+++ terraform/main.tf
@@ -0,0 +1,47 @@
+provider "azurerm" {
+  features {}
+}
+
+# Resource Group
+resource "azurerm_resource_group" "aks" {
+  name     = var.resource_group_name
+  location = var.location
+
+  tags = var.tags
+}
+
+# AKS Cluster
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.cluster_name
+  location            = azurerm_resource_group.aks.location
+  resource_group_name = azurerm_resource_group.aks.name
+  dns_prefix          = var.dns_prefix
+  kubernetes_version  = var.kubernetes_version
+
+  default_node_pool {
+    name                         = var.default_node_pool_name
+    node_count                   = var.node_count
+    vm_size                      = var.vm_size
+    enable_auto_scaling          = var.enable_auto_scaling
+    min_count                    = var.enable_auto_scaling ? var.min_count : null
+    max_count                    = var.enable_auto_scaling ? var.max_count : null
+    os_disk_type                 = "Ephemeral"
+    enable_host_encryption       = true
+    os_disk_size_gb              = var.os_disk_size_gb
+    only_critical_addons_enabled = true
+    type                         = "VirtualMachineScaleSets"
+    tags                         = var.tags
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin    = var.network_plugin
+    load_balancer_sku = "standard"
+    network_policy    = var.network_policy
+  }
+
+  tags = var.tags
+}
